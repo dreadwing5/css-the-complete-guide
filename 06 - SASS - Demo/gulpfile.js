c1b54f7b6324d9db9eff5a11494c81958a94805a
dreadwing5 <sachinspyk@outlook.com>
2021-09-14 18:36:42 +0530

    removed unused css using purgecss

--- 06 - SASS - Demo/gulpfile.js
+++ 06 - SASS - Demo/gulpfile.js
@@ -1,14 +1,23 @@
-const { src, dest, watch, series } = require('gulp')
-const sass = require('gulp-sass')(require('sass'));
+const {
+  src,
+  dest,
+  watch,
+  series
+} = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
-  return src('sass/**/*.scss')
+  return src("sass/**/*.scss")
     .pipe(sass())
-    .pipe(dest('css'))
+    .pipe(purgecss({
+      content: ['*.html']
+    }))
+    .pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(['sass/**/*.scss'], buildStyles)
+  watch(["sass/**/*.scss", "*.html"], buildStyles);
 }
 
-exports.default = series(buildStyles, watchTask)
+exports.default = series(buildStyles, watchTask);
